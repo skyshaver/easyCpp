96886a096c43f288091a49fe1f348c2fe18ad44e
Sky Shaver <skyshaver@me.com>
2021-05-09 16:16:12 -0400

    adds basic model for questions
    adds model to testsToJson
    updates warm_up to make entries easier to parse

---
+++ testsToJson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testsToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
